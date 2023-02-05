1c3405b83c12d7652e0d6c994dc103a89f63399d
distek <distek@protonmail.com>
2023-02-05 10:43:35 -0800

    Changed to work with neovim plugin distek/sessions.nvim

--- scripts/session-open.sh
+++ scripts/session-open.sh
@@ -1,20 +1,39 @@
 #!/usr/bin/env bash
 
-projects=($(projects dump))
+sessionsDir=~/.local/share/nvim/sessions
 
-projectName=$(for p in ${projects[@]}; do 
-    echo $p | cut -d',' -f1
+if ! cd "$sessionsDir"; then
+	echo "Could not cd into $sessionsDir"
+	exit 1
+fi
+
+projects=($(for i in $(ls -1 --sort=time); do
+	printf "%s:%s\n" "$i" $(grep -i '" name:' $i | sed 's/"\ name://')
+done))
+
+projectName=$(for p in ${projects[@]}; do
+	echo $p | cut -d':' -f2
 done | fzf \
---bind="esc:reload(echo)" \
---bind=esc:abort)
+	--layout=reverse \
+	--header="Open session" \
+	--header-first \
+	--bind="esc:reload(echo)" \
+	--bind=esc:abort)
 
 if [ ! -z projectName ] && [ ! $projectName = "" ]; then
-    for i in ${projects[@]}; do
-        project=($(echo $i | sed "s/,/ /g"))
-        if [ ${project[0]} = $projectName ]; then
-            projectI=$(tmux ls | grep ${project[0]} | wc -l)
-            tmux new-session -c "${project[1]}" -s ${project[0]}$projectI -d
-            tmux switch-client -t "${project[0]}$projectI"
-        fi
-    done
+	for i in ${projects[@]}; do
+		project=($(echo $i | sed "s/:/ /g"))
+
+		if [ ${project[1]} = $projectName ]; then
+			projectI=$(tmux ls | grep ${project[1]} | wc -l)
+
+			projectDir=$(grep -i '" cwd:' ${project[0]} | sed 's/"\ cwd://')
+
+			tmux new-session -c "$projectDir" -s ${project[1]}$projectI -d
+
+			tmux send-keys -t "${project[1]}$projectI" " vim -c \"SessionLoad ${project[1]}\"" "Enter"
+
+			tmux switch-client -t "${project[1]}$projectI"
+		fi
+	done
 fi
